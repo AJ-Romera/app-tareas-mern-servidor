e608b4e763ba893204e8faaae04a03e3a99de5d0
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-24 18:56:38 +0100

    CHNG: Crea el Servidor de Express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Probando desde index.js');
+const express = require('express');
+
+// Crear el servidor
+const app = express();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+/* // Definir la página principal
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+}); */
+
+// Arrancar la app/servidor
+app.listen(PORT, () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
