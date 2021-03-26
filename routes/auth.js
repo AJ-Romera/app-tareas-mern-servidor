594d636b3c7faf0674e9e51d08e77bd7e926b754
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-26 17:31:47 +0100

    CHNG: Crea Ruta && Controller de Autenticación

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+// Rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const { check } = require('express-validator');
+
+// Crea un usuario
+// api/usuarios
+router.post(
+    '/',
+    [
+        check('email', 'Por favor, introduzca un email válido').isEmail(),
+        check(
+            'password',
+            'El password debe ser de al menos 6 caracteres'
+        ).isLength({ min: 6 }),
+    ],
+    authController.autenticarUsuario
+);
+
+module.exports = router;
