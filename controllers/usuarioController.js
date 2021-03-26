e61ab5adc2e0ad5c71364d5f638f710a400ffa10
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-26 16:50:30 +0100

    CHNG: Crea un JWT y lo Firma

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,7 @@
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario = async (req, res) => {
     // Revisar si hay errores
@@ -30,8 +31,27 @@ exports.crearUsuario = async (req, res) => {
         // Guardar usuario
         await usuario.save();
 
-        // Mensaje de confirmación
-        res.json({ msg: 'Usuario creado correctamente' });
+        // Crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id,
+            },
+        };
+
+        // Firmar el JWT
+        jwt.sign(
+            payload,
+            process.env.SECRETO,
+            {
+                expiresIn: 3600, // 3600s = 60 minutos = 1 Hora
+            },
+            (error, token) => {
+                if (error) throw error;
+
+                // Mensaje de confirmación
+                res.json({ token });
+            }
+        );
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
