774062214e812e39672d9ef96ba43d35549619e9
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-29 17:52:16 +0200

    CHNG: Autentica si el Par Usuario/Password es Correcto

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,4 +9,46 @@ exports.autenticarUsuario = async (req, res) => {
     if (!errores.isEmpty()) {
         return res.status(400).json({ errores: errores.array() });
     }
+
+    // Extraer el email y el password
+    const { email, password } = req.body;
+
+    try {
+        // Revisar que sea un usuario registrado
+        let usuario = await Usuario.findOne({ email });
+        if (!usuario) {
+            return res.status(400).json({ msg: 'El usuario no existe' });
+        }
+
+        // Revisar el password
+        const passCorrecto = await bcrypt.compare(password, usuario.password);
+        if (!passCorrecto) {
+            return res.status(400).json({ msg: 'Password Incorrecto' });
+        }
+
+        // Si todo es correcto:
+        // Crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id,
+            },
+        };
+
+        // Firmar el JWT
+        jwt.sign(
+            payload,
+            process.env.SECRETO,
+            {
+                expiresIn: 3600, // 3600s = 60 minutos = 1 Hora
+            },
+            (error, token) => {
+                if (error) throw error;
+
+                // Mensaje de confirmación
+                res.json({ token });
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
