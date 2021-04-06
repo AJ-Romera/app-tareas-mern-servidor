dedc6db95c4140e5736b4a7fc777323cfe692147
AJ-Romera <ajromeraalfonso@gmail.com>
2021-04-06 13:06:13 +0200

    FIX: Elimina Validación porque ya la Hay en el Cliente

--- routes/auth.js
+++ routes/auth.js
@@ -2,22 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const { check } = require('express-validator');
+/* const { check } = require('express-validator'); */
 const auth = require('../middleware/auth');
 
 // Iniciar sesión
 // api/auth
-router.post(
-    '/',
-    [
-        check('email', 'Por favor, introduzca un email válido').isEmail(),
-        check(
-            'password',
-            'El password debe ser de al menos 6 caracteres'
-        ).isLength({ min: 6 }),
-    ],
-    authController.autenticarUsuario
-);
+router.post('/', authController.autenticarUsuario);
 
 // Obtiene el usuario autenticado
 router.get('/', auth, authController.usuarioAutenticado);
